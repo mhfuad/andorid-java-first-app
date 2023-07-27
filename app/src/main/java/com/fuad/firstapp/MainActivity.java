d02f335d128821a39971b15c472fecca43a6b8a2
mhfuad <mhfuad2@gmail.com>
2023-07-27 16:25:50 +0600

    form add

--- app/src/main/java/com/fuad/firstapp/MainActivity.java
+++ app/src/main/java/com/fuad/firstapp/MainActivity.java
@@ -4,43 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
     public static final String API_URL = "https://64a40253c3b509573b56ea44.mockapi.io";
-    //TextView textViewResult;
     RecyclerView rcvMain;
+    FloatingActionButton fab;
     List<Post> posts;
 
     @Override
@@ -48,35 +26,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //textViewResult = findViewById(R.id.text_view_result);
         rcvMain = findViewById(R.id.rcvMain);
+        fab = findViewById(R.id.fab);
         rcvMain.setLayoutManager(new LinearLayoutManager(this));
+
+
+        fab.setContentDescription("Add new post");
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "button click",Toast.LENGTH_LONG).show();
+                openPostForm();
+            }
+        });
 
         RetrofitInstance.getInstance().mockApi.getPosts().enqueue(new Callback<List<Post>>() {
             @Override
             public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
                 if(!response.isSuccessful()){
-                    //textViewResult.setText("code: "+ response.code());
                     return;
                 }
 
                 posts = response.body();
                 rcvMain.setAdapter(new PostAdapter(MainActivity.this,posts));
-                for(Post post: posts){
-                    String content = "";
-                    content += "Id: "+ post.getId() + "\n";
-                    content += "User ID: "+ post.getUserId() + "\n";
-                    content += "Title: "+ post.getTitle() + "\n";
-                    content += "Text: "+ post.getText() + "\n";
-                    //textViewResult.append(content);
-                }
-
             }
 
             @Override
             public void onFailure(Call<List<Post>> call, Throwable t) {
-                //textViewResult.setText("prob "+t.getMessage());
             }
         });
+    }
+    public void openPostForm(){
+        Intent intent = new Intent(this, AddPostActivity.class);
+        startActivity(intent);
     }
 }
