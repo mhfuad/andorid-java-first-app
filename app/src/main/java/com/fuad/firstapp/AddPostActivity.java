ff73a246aa989700faecde5d54f209fbcbb0992b
mhfuad <mhfuad2@gmail.com>
2023-08-01 19:20:29 +0600

    post add

--- app/src/main/java/com/fuad/firstapp/AddPostActivity.java
+++ app/src/main/java/com/fuad/firstapp/AddPostActivity.java
@@ -2,13 +2,70 @@ package com.fuad.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AddPostActivity extends AppCompatActivity {
+
+    EditText title, description;
+    Button postSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
+
+        title = findViewById(R.id.title);
+        description = findViewById(R.id.description);
+        postSubmit = findViewById(R.id.postSubmit);
+
+        postSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PostRequest postRequest = new PostRequest();
+
+
+                if(title.getText().length() != 0 && description.getText().length() != 0) {
+                    postRequest.setTitle(title.getText().toString());
+                    postRequest.setBody(description.getText().toString());
+
+                    RetrofitInstance.getInstance().mockApi.savePost(postRequest).enqueue(new Callback<Post>() {
+                        @Override
+                        public void onResponse(Call<Post> call, Response<Post> response) {
+                            if (response.isSuccessful()) {
+                                Toast.makeText(AddPostActivity.this, "Save Post Successful", Toast.LENGTH_LONG).show();
+                                gotoMain();
+                            } else {
+                                Toast.makeText(AddPostActivity.this, "Save Post Fail", Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<Post> call, Throwable t) {
+                            Toast.makeText(AddPostActivity.this, "Save Post fail" + t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }else if(title.getText().length() == 0){
+                    Toast.makeText(AddPostActivity.this, "Title should not empty", Toast.LENGTH_LONG).show();
+                }else if (description.getText().length() == 0){
+                    Toast.makeText(AddPostActivity.this, "Description should not empty", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void gotoMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/fuad/firstapp/MockApi.java
+++ app/src/main/java/com/fuad/firstapp/MockApi.java
@@ -3,9 +3,14 @@ package com.fuad.firstapp;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface MockApi {
     @GET("/post")
     Call<List<Post>> getPosts();
+
+    @POST("/post")
+    Call<Post> savePost(@Body PostRequest postRequest);
 }
